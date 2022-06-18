90e4f2d49060ccbb8ba54ac154a37e20fb6adeaf
dejazer <dejazer@gmail.com>
2022-06-18 21:16:16 +0200

    Changes slide on press key 'LEFT' and 'RIGHT'

--- main.js
+++ main.js
@@ -41,3 +41,23 @@ const changeDot = () => {
 }
 
 let intervalIndex = setInterval(changeSlide, 2000);
+const keyChangeSlide = e => {
+
+   if (e.code == 'ArrowLeft' || e.code == 'ArrowRight') {
+      clearInterval(intervalIndex);
+      e.code == 'ArrowLeft' ? active-- : active++;
+      if (active < 0) {
+         active = articles.length - 1;
+      }
+      if (active == articles.length) {
+         active = 0;
+      }
+
+      img.src = articles[active].image;
+      h1.textContent = articles[active].title;
+      changeDot()
+      intervalIndex = setInterval(changeSlide, 2000);
+   }
+}
+
+window.addEventListener('keydown', keyChangeSlide)
